9d01a4151a6e756b93aa88f537f2f1865ed4b974
Tamal Saha <tamal@appscode.com>
2016-12-21 19:14:30 -0800

    Add Docker build script.

---
+++ hack/docker/setup.sh
@@ -0,0 +1,92 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+LIB_ROOT=$(dirname "${BASH_SOURCE}")/..
+source "$LIB_ROOT/libbuild/common/lib.sh"
+source "$LIB_ROOT/libbuild/common/public_image.sh"
+
+GOPATH=$(go env GOPATH)
+SRC=$GOPATH/src
+BIN=$GOPATH/bin
+ROOT=$GOPATH
+
+APPSCODE_ENV=${APPSCODE_ENV:-dev}
+IMG=voyager
+
+DIST=$GOPATH/src/github.com/appscode/voyager/dist
+mkdir -p $DIST
+if [ -f "$DIST/.tag" ]; then
+	export $(cat $DIST/.tag | xargs)
+fi
+
+clean() {
+    pushd $GOPATH/src/github.com/appscode/voyager/hack/docker
+	rm -rf voyager
+	popd
+}
+
+build_binary() {
+	pushd $GOPATH/src/github.com/appscode/voyager
+	./hack/builddeps.sh
+    ./hack/make.py build voyager
+	detect_tag $DIST/.tag
+	popd
+}
+
+build_docker() {
+	pushd $GOPATH/src/github.com/appscode/voyager/hack/docker
+	cp $DIST/voyager/voyager-linux-amd64 voyager
+	chmod 755 voyager
+
+	cat >Dockerfile <<EOL
+FROM appscode/base:8.6
+
+RUN set -x \
+  && apt-get update \
+  && apt-get install -y --no-install-recommends ca-certificates \
+  && rm -rf /var/lib/apt/lists/* /usr/share/doc /usr/share/man /tmp/*
+
+COPY voyager /voyager
+EOL
+	local cmd="docker build -t appscode/$IMG:$TAG ."
+	echo $cmd; $cmd
+
+	rm voyager Dockerfile
+	popd
+}
+
+build() {
+	build_binary
+	build_docker
+}
+
+docker_push() {
+	if [ "$APPSCODE_ENV" = "prod" ]; then
+		echo "Nothing to do in prod env. Are you trying to 'release' binaries to prod?"
+		exit 0
+	fi
+
+    if [[ "$(docker images -q appscode/$IMG:$TAG 2> /dev/null)" != "" ]]; then
+        docker_up $IMG:$TAG
+    fi
+}
+
+docker_release() {
+	if [ "$APPSCODE_ENV" != "prod" ]; then
+		echo "'release' only works in PROD env."
+		exit 1
+	fi
+	if [ "$TAG_STRATEGY" != "git_tag" ]; then
+		echo "'apply_tag' to release binaries and/or docker images."
+		exit 1
+	fi
+
+    if [[ "$(docker images -q appscode/$IMG:$TAG 2> /dev/null)" != "" ]]; then
+        docker push appscode/$IMG:$TAG
+    fi
+}
+
+source_repo $@
