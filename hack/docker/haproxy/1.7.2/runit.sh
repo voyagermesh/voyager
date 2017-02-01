798ddf6c9e616a5fc9231213cbed3464ea0f3f5a
tamal <tamal@appscode.com>
2017-02-01 05:41:52 -0800

    added-all

--- hack/docker/haproxy/1.7.2/runit.sh
+++ hack/docker/haproxy/1.7.2/runit.sh
@@ -4,6 +4,9 @@ export > /etc/envvars
 
 [[ $DEBUG == true ]] && set -x
 
+# propagate kloader args
+sed -i "s/__KLOADER_ARGS__/$@/g" /etc/sv/reloader/run
+
 CERT_DIR=/etc/ssl/private/haproxy
 mkdir -p /etc/ssl/private/haproxy
 
