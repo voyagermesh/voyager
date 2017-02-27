4bb2f524b40c5b1e6d8b463d4f493144d7d9e7e1
tamal <tamal@appscode.com>
2017-02-27 05:16:39 -0800

    Pass KLOADER_ARGS as env variable.

--- hack/docker/haproxy/1.7.2/runit.sh
+++ hack/docker/haproxy/1.7.2/runit.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
+export KLOADER_ARGS="$@"
 export > /etc/envvars
 
 [[ $DEBUG == true ]] && set -x
-
-# propagate kloader args
-sed -i "s|__KLOADER_ARGS__|$@|g" /etc/sv/reloader/run
 
 CERT_DIR=/etc/ssl/private/haproxy
 mkdir -p /etc/ssl/private/haproxy
