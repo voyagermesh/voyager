b86abb1bda415d45e44cb817ed2e5741366e66cb
Sadlil Rhythom <sadlil.rhythm@gmail.com>
2017-03-01 13:53:37 +0600

    Remove ALPN h2 for https. (#33)

--- pkg/controller/ingress/template/template.go
+++ pkg/controller/ingress/template/template.go
@@ -86,7 +86,7 @@ backend default-backend
 {% if HttpsService %}
 # https service
 frontend https-frontend
-    bind *:443 ssl no-sslv3 no-tlsv10 crt /etc/ssl/private/haproxy/ alpn h2,http/1.1
+    bind *:443 ssl no-sslv3 no-tlsv10 crt /etc/ssl/private/haproxy/ alpn http/1.1
     rspadd  Strict-Transport-Security:\ max-age=15768000
 
     mode http
