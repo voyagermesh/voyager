e18d282d87313b10f043c50c4f08736a961b8e70
tamal <tamal@appscode.com>
2017-05-16 18:13:57 -0700

    added-all

---
+++ hack/docker/haproxy/1.7.5/runit.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export KLOADER_ARGS="$@"
+export > /etc/envvars
+
+[[ $DEBUG == true ]] && set -x
+
+# create haproxy.cfg dir
+mkdir /etc/haproxy
+
+CERT_DIR=/etc/ssl/private/haproxy
+mkdir -p /etc/ssl/private/haproxy
+
+# http://stackoverflow.com/a/2108296
+for dir in /srv/haproxy/secrets/*/
+do
+	# remove trailing /
+	dir=${dir%*/}
+	# just basename
+	secret=${dir##*/}
+
+	cat $dir/tls.crt >  $CERT_DIR/$secret.pem
+	cat $dir/tls.key >> $CERT_DIR/$secret.pem
+done
+
+echo "Starting runit..."
+exec /usr/sbin/runsvdir-start
---
+++ hack/docker/haproxy/1.7.5/setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+GOPATH=$(go env GOPATH)
+REPO_ROOT=$GOPATH/src/github.com/appscode/voyager
+
+source "$REPO_ROOT/hack/libbuild/common/public_image.sh"
+
+detect_tag $REPO_ROOT/dist/.tag
+
+IMG=haproxy
+TAG=1.7.5-$TAG
+
+build() {
+	pushd $(dirname "${BASH_SOURCE}")
+	wget -O kloader https://cdn.appscode.com/binaries/kloader/1.5.0/kloader-linux-amd64
+	chmod +x kloader
+	local cmd="docker build -t appscode/$IMG:$TAG ."
+	echo $cmd; $cmd
+	rm kloader
+	popd
+}
+
+binary_repo $@
