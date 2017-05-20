55e6ead87dbaad462ac6442f15987a791b3a05e1
Tamal Saha <tamal@appscode.com>
2017-05-20 08:28:18 -0700

    Update setup.sh

--- hack/docker/voyager/setup.sh
+++ hack/docker/voyager/setup.sh
@@ -43,9 +43,9 @@ build_docker() {
 FROM alpine
 
 RUN set -x \
-    && apk --update upgrade \
-    && apk add ca-certificates \
-    && rm -rf /var/cache/apk/*
+  && apk update \
+  && apk add ca-certificates \
+  && rm -rf /var/cache/apk/*
 
 COPY voyager /voyager
 USER nobody:nobody
