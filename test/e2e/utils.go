fc978df75901888ff8b51caba620a8b345c1cc35
tamal <tamal@appscode.com>
2017-06-08 17:27:31 -0700

    added-all

--- test/e2e/utils.go
+++ test/e2e/utils.go
@@ -101,9 +101,7 @@ func (s *IngressTestSuit) getURLs(baseIngress *api.Ingress) ([]string, error) {
 func (s *IngressTestSuit) getDaemonURLs(baseIngress *api.Ingress) ([]string, error) {
 	serverAddr := make([]string, 0)
 	nodes, err := s.t.KubeClient.Core().Nodes().List(kapi.ListOptions{
-		LabelSelector: labels.SelectorFromSet(
-			api.ParseNodeSelector(baseIngress.Annotations[api.NodeSelector]),
-		),
+		LabelSelector: labels.SelectorFromSet(baseIngress.NodeSelector()),
 	})
 	if err != nil {
 		return nil, errors.New().WithCause(err).Err()
