672dbe14c43f6d382fd90a465eb8ff8b8aba23d2
tamal <tamal@appscode.com>
2017-06-18 02:42:56 -0700

    Add HAProxy 1.7.6 dockerfiles.

---
+++ hack/docker/haproxy/1.7.6/runit.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+export KLOADER_ARGS="$@"
+export > /etc/envvars
+
+[[ $DEBUG == true ]] && set -x
+
+# create haproxy.cfg dir
+mkdir /etc/haproxy
+
+CERT_DIR=/etc/ssl/private/haproxy
+mkdir -p /etc/ssl/private/haproxy
+
+# http://stackoverflow.com/a/2108296
+for dir in /srv/haproxy/secrets/*/
+do
+	# remove trailing /
+	dir=${dir%*/}
+	# just basename
+	secret=${dir##*/}
+
+	cat $dir/tls.crt >  $CERT_DIR/$secret.pem
+	cat $dir/tls.key >> $CERT_DIR/$secret.pem
+done
+
+echo "Checking HAProxy configuration ..."
+cmd="/kloader check $KLOADER_ARGS"
+echo $cmd
+$cmd
+rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
+
+echo "Starting runit..."
+exec /usr/sbin/runsvdir-start
