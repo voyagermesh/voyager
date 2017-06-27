30a50ad62a3c2109a2aff099b6c3926ff3096f9d
tamal <tamal@appscode.com>
2017-06-27 05:04:11 -0700

    added-all

--- hack/builddeps.sh
+++ hack/builddeps.sh
@@ -5,3 +5,7 @@ pip install git+https://github.com/ellisonbg/antipackage.git#egg=antipackage
 pip install pyyaml
 
 go get -u golang.org/x/tools/cmd/goimports
+go get github.com/Masterminds/glide
+go get github.com/sgotti/glide-vc
+go get github.com/onsi/ginkgo/ginkgo
+go install github.com/onsi/ginkgo/ginkgo
