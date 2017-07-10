6f6732dca1a3e2a38b7a7f5fc9ee4fa26a00e23a
tamal <tamal@appscode.com>
2017-07-09 17:42:06 -0700

    added-all

---
+++ pkg/operator/ingress_tprs.go
@@ -0,0 +1,60 @@
+package operator
+
+import (
+	"errors"
+	"fmt"
+
+	acrt "github.com/appscode/go/runtime"
+	"github.com/appscode/log"
+	sapi "github.com/appscode/voyager/api"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/apimachinery/pkg/watch"
+	apiv1 "k8s.io/client-go/pkg/api/v1"
+	"k8s.io/client-go/tools/cache"
+)
+
+// Blocks caller. Intended to be called as a Go routine.
+func (c *Operator) WatchIngressTPRs() {
+	defer acrt.HandleCrash()
+
+	lw := &cache.ListWatch{
+		ListFunc: func(opts metav1.ListOptions) (runtime.Object, error) {
+			return c.ExtClient.Ingresses(apiv1.NamespaceAll).List(metav1.ListOptions{})
+		},
+		WatchFunc: func(options metav1.ListOptions) (watch.Interface, error) {
+			return c.ExtClient.Ingresses(apiv1.NamespaceAll).Watch(metav1.ListOptions{})
+		},
+	}
+	_, ctrl := cache.NewInformer(lw,
+		&sapi.Ingress{},
+		c.SyncPeriod,
+		cache.ResourceEventHandlerFuncs{
+			AddFunc: func(obj interface{}) {
+				if resource, ok := obj.(*sapi.Ingress); ok {
+					fmt.Println(resource.Name)
+				}
+			},
+			UpdateFunc: func(old, new interface{}) {
+				oldObj, ok := old.(*sapi.Ingress)
+				if !ok {
+					log.Errorln(errors.New("Invalid Ingress object"))
+					return
+				}
+				newObj, ok := new.(*sapi.Ingress)
+				if !ok {
+					log.Errorln(errors.New("Invalid Ingress object"))
+					return
+				}
+				fmt.Println(oldObj.Name, newObj.Name)
+			},
+			DeleteFunc: func(obj interface{}) {
+				if resource, ok := obj.(*sapi.Ingress); ok {
+					fmt.Println(resource.Name)
+				}
+			},
+		},
+	)
+	ctrl.Run(wait.NeverStop)
+}
