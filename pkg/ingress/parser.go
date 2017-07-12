b675294c6a1928f26c529c76dcb18d0c0f8709cf
tamal <tamal@appscode.com>
2017-07-11 19:56:26 -0700

    added-all

--- pkg/ingress/parser.go
+++ pkg/ingress/parser.go
@@ -12,12 +12,61 @@ import (
 	stringutil "github.com/appscode/go/strings"
 	"github.com/appscode/log"
 	"github.com/appscode/voyager/api"
+	_ "github.com/appscode/voyager/api/install"
+	acs "github.com/appscode/voyager/client/clientset"
+	"github.com/appscode/voyager/pkg/config"
 	"github.com/appscode/voyager/pkg/ingress/template"
+	"github.com/appscode/voyager/third_party/forked/cloudprovider"
+	_ "github.com/appscode/voyager/third_party/forked/cloudprovider/providers"
+	fakecloudprovider "github.com/appscode/voyager/third_party/forked/cloudprovider/providers/fake"
+	pcm "github.com/coreos/prometheus-operator/pkg/client/monitoring/v1alpha1"
 	"github.com/flosch/pongo2"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
+	clientset "k8s.io/client-go/kubernetes"
 	apiv1 "k8s.io/client-go/pkg/api/v1"
 )
+
+func NewController(
+	kubeClient clientset.Interface,
+	extClient acs.ExtensionInterface,
+	promClient pcm.MonitoringV1alpha1Interface,
+	opt config.Options,
+	ingress *api.Ingress) *Controller {
+	h := &Controller{
+		KubeClient: kubeClient,
+		ExtClient:  extClient,
+		PromClient: promClient,
+		Opt:        opt,
+		Resource:   ingress,
+	}
+	log.Infoln("Initializing cloud manager for provider", opt.CloudProvider)
+	if opt.CloudProvider == "aws" || opt.CloudProvider == "gce" || opt.CloudProvider == "azure" {
+		cloudInterface, err := cloudprovider.InitCloudProvider(opt.CloudProvider, opt.CloudConfigFile)
+		if err != nil {
+			log.Errorln("Failed to initialize cloud provider:"+opt.CloudProvider, err)
+		} else {
+			log.Infoln("Initialized cloud provider: "+opt.CloudProvider, cloudInterface)
+			h.CloudManager = cloudInterface
+		}
+	} else if opt.CloudProvider == "gke" {
+		cloudInterface, err := cloudprovider.InitCloudProvider("gce", opt.CloudConfigFile)
+		if err != nil {
+			log.Errorln("Failed to initialize cloud provider:"+opt.CloudProvider, err)
+		} else {
+			log.Infoln("Initialized cloud provider: "+opt.CloudProvider, cloudInterface)
+			h.CloudManager = cloudInterface
+		}
+	} else if opt.CloudProvider == "minikube" {
+		h.CloudManager = &fakecloudprovider.FakeCloud{}
+	} else {
+		log.Infoln("No cloud manager found for provider", opt.CloudProvider)
+	}
+
+	h.parseOptions()
+	h.parseSpec()
+	return h
+}
 
 func (lbc *Controller) SupportsLBType() bool {
 	switch lbc.Resource.LBType() {
--- pkg/ingress/types.go
+++ pkg/ingress/types.go
@@ -3,14 +3,10 @@ package ingress
 import (
 	"sync"
 
-	"github.com/appscode/log"
 	"github.com/appscode/voyager/api"
-	_ "github.com/appscode/voyager/api/install"
 	acs "github.com/appscode/voyager/client/clientset"
 	"github.com/appscode/voyager/pkg/config"
 	"github.com/appscode/voyager/third_party/forked/cloudprovider"
-	_ "github.com/appscode/voyager/third_party/forked/cloudprovider/providers"
-	fakecloudprovider "github.com/appscode/voyager/third_party/forked/cloudprovider/providers/fake"
 	pcm "github.com/coreos/prometheus-operator/pkg/client/monitoring/v1alpha1"
 	clientset "k8s.io/client-go/kubernetes"
 )
@@ -106,45 +102,4 @@ type Endpoint struct {
 	UseDNSResolver bool
 	DNSResolver    string
 	CheckHealth    bool
-}
-
-func NewController(
-	kubeClient clientset.Interface,
-	extClient acs.ExtensionInterface,
-	promClient pcm.MonitoringV1alpha1Interface,
-	opt config.Options,
-	ingress *api.Ingress) *Controller {
-	h := &Controller{
-		KubeClient: kubeClient,
-		ExtClient:  extClient,
-		PromClient: promClient,
-		Opt:        opt,
-		Resource:   ingress,
-	}
-	log.Infoln("Initializing cloud manager for provider", opt.CloudProvider)
-	if opt.CloudProvider == "aws" || opt.CloudProvider == "gce" || opt.CloudProvider == "azure" {
-		cloudInterface, err := cloudprovider.InitCloudProvider(opt.CloudProvider, opt.CloudConfigFile)
-		if err != nil {
-			log.Errorln("Failed to initialize cloud provider:"+opt.CloudProvider, err)
-		} else {
-			log.Infoln("Initialized cloud provider: "+opt.CloudProvider, cloudInterface)
-			h.CloudManager = cloudInterface
-		}
-	} else if opt.CloudProvider == "gke" {
-		cloudInterface, err := cloudprovider.InitCloudProvider("gce", opt.CloudConfigFile)
-		if err != nil {
-			log.Errorln("Failed to initialize cloud provider:"+opt.CloudProvider, err)
-		} else {
-			log.Infoln("Initialized cloud provider: "+opt.CloudProvider, cloudInterface)
-			h.CloudManager = cloudInterface
-		}
-	} else if opt.CloudProvider == "minikube" {
-		h.CloudManager = &fakecloudprovider.FakeCloud{}
-	} else {
-		log.Infoln("No cloud manager found for provider", opt.CloudProvider)
-	}
-
-	h.parseOptions()
-	h.parseSpec()
-	return h
 }
