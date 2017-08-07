da8b2e20d4f6d6c4fbc47d653e75a69b2b7f9ba7
tamal <tamal@appscode.com>
2017-08-07 16:40:07 -0700

    added-all

--- pkg/haproxy/template.go
+++ pkg/haproxy/template.go
@@ -97,155 +97,155 @@ defaults
 
 	_ = template.Must(template.New("dns-resolver").Funcs(funcMap).Parse(`
 resolvers {{ .Name }}
-{{ range $index, $ns := .NameServer }}
-nameserver dns{{ $index }} {{ $ns }}
-{{ end }}
-{{ if .Retries }}
-resolve_retries {{ .Retries }}
-{{ end }}
-{{ range $event, $time := .Timeout }}
-timeout {{ $event }} {{ $time }}
-{{ end }}
-{{ range $status, $period := .Hold }}
-hold {{ $status }} {{ $period }}
-{{ end }}
+	{{ range $index, $ns := .NameServer }}
+	nameserver dns{{ $index }} {{ $ns }}
+	{{ end }}
+	{{ if .Retries }}
+	resolve_retries {{ .Retries }}
+	{{ end }}
+	{{ range $event, $time := .Timeout }}
+	timeout {{ $event }} {{ $time }}
+	{{ end }}
+	{{ range $status, $period := .Hold }}
+	hold {{ $status }} {{ $period }}
+	{{ end }}
 `))
 
 	_ = template.Must(template.New("stats").Funcs(funcMap).Parse(`
 listen stats
-bind *:{{ .Port }}
-mode http
-stats enable
-stats realm Haproxy\ Statistics
-stats uri /
-{{ if .Username }}stats auth {{ .Username }}:{{ .PassWord }}{{ end }}
+	bind *:{{ .Port }}
+	mode http
+	stats enable
+	stats realm Haproxy\ Statistics
+	stats uri /
+	{{ if .Username }}stats auth {{ .Username }}:{{ .PassWord }}{{ end }}
 `))
 
 	_ = template.Must(template.New("default-frontend").Funcs(funcMap).Parse(`
 frontend http-frontend
-    bind *:80 {{ if .AcceptProxy }}accept-proxy{{ end }}
+	bind *:80 {{ if .AcceptProxy }}accept-proxy{{ end }}
 
-    mode http
-    option httplog
-    option forwardfor
+	mode http
+	option httplog
+	option forwardfor
 
-    default_backend default-backend
+	default_backend default-backend
 `))
 
 	_ = template.Must(template.New("default-backend").Funcs(funcMap).Parse(`
 backend default-backend
-{{ if .Sticky }}cookie SERVERID insert indirect nocache{{ end }}
+	{{ if .Sticky }}cookie SERVERID insert indirect nocache{{ end }}
 
-{{ range $rule := .DefaultBackend.BackendRules }}
-{{ $rule }}
-{{ end }}
+	{{ range $rule := .DefaultBackend.BackendRules }}
+	{{ $rule }}
+	{{ end }}
 
-{{ range $rule := .DefaultBackend.RewriteRules }}
-reqrep {{ $rule }}
-{{ end }}
+	{{ range $rule := .DefaultBackend.RewriteRules }}
+	reqrep {{ $rule }}
+	{{ end }}
 
-{{ range $index, $rule := .DefaultBackend.HeaderRules }}
-acl ___header_x_{{ $index }}_exists req.hdr({{ $rule | header_name }}) -m found
-http-request add-header {{ $rule }} unless ___header_x_{{ $index }}_exists
-{{ end }}
+	{{ range $index, $rule := .DefaultBackend.HeaderRules }}
+	acl ___header_x_{{ $index }}_exists req.hdr({{ $rule | header_name }}) -m found
+	http-request add-header {{ $rule }} unless ___header_x_{{ $index }}_exists
+	{{ end }}
 
-{{ range $e := .DefaultBackend.Endpoints }}
-{{ if $e.ExternalName }}
-{{ if $e.UseDNSResolver }}
-server {{ $e.Name }} {{ $e.ExternalName }}:{{ $e.Port }} {{ if $e.DNSResolver }} {{ if $e.CheckHealth }} check {{ end }} resolvers {{ $e.DNSResolver }} resolve-prefer ipv4 {{ end }}
-{{ else if not .DefaultBackend.BackendRules }}
-acl https ssl_fc
-http-request redirect location https://{{$e.ExternalName}}:{{ $e.Port }} code 301 if https
-http-request redirect location http://{{$e.ExternalName}}:{{ $e.Port }} code 301 unless https
-{{ end }}
-{{ else }}
-server {{ $e.Name }} {{ $e.IP }}:{{ $e.Port }} {{ if $e.Weight }}weight {{ $e.Weight }}{{ end }} {{ if .Sticky }}cookie {{ $e.Name }}{{ end }}
-{{ end }}
-{{ end }}
+	{{ range $e := .DefaultBackend.Endpoints }}
+	{{ if $e.ExternalName }}
+	{{ if $e.UseDNSResolver }}
+	server {{ $e.Name }} {{ $e.ExternalName }}:{{ $e.Port }} {{ if $e.DNSResolver }} {{ if $e.CheckHealth }} check {{ end }} resolvers {{ $e.DNSResolver }} resolve-prefer ipv4 {{ end }}
+	{{ else if not .DefaultBackend.BackendRules }}
+	acl https ssl_fc
+	http-request redirect location https://{{$e.ExternalName}}:{{ $e.Port }} code 301 if https
+	http-request redirect location http://{{$e.ExternalName}}:{{ $e.Port }} code 301 unless https
+	{{ end }}
+	{{ else }}
+	server {{ $e.Name }} {{ $e.IP }}:{{ $e.Port }} {{ if $e.Weight }}weight {{ $e.Weight }}{{ end }} {{ if .Sticky }}cookie {{ $e.Name }}{{ end }}
+	{{ end }}
+	{{ end }}
 `))
 
 	_ = template.Must(template.New("http-frontend").Funcs(funcMap).Parse(`
 frontend {{ .FrontendName }}
-{{ if .UseSSL }}
-bind *:{{ .Port }} {{ if .AcceptProxy }}accept-proxy{{ end }} ssl no-sslv3 no-tlsv10 no-tls-tickets crt /etc/ssl/private/haproxy/ alpn http/1.1
-# Mark all cookies as secure
-rsprep ^Set-Cookie:\ (.*) Set-Cookie:\ \1;\ Secure
-# Add the HSTS header with a 6 month max-age
-rspadd  Strict-Transport-Security:\ max-age=15768000
-{{ else }}
-bind *:{{ .Port }} {{ if .AcceptProxy }}accept-proxy{{ end }}
-{{ end }}
+	{{ if .UseSSL }}
+	bind *:{{ .Port }} {{ if .AcceptProxy }}accept-proxy{{ end }} ssl no-sslv3 no-tlsv10 no-tls-tickets crt /etc/ssl/private/haproxy/ alpn http/1.1
+	# Mark all cookies as secure
+	rsprep ^Set-Cookie:\ (.*) Set-Cookie:\ \1;\ Secure
+	# Add the HSTS header with a 6 month max-age
+	rspadd  Strict-Transport-Security:\ max-age=15768000
+	{{ else }}
+	bind *:{{ .Port }} {{ if .AcceptProxy }}accept-proxy{{ end }}
+	{{ end }}
 
-mode http
-option httplog
-option forwardfor
+	mode http
+	option httplog
+	option forwardfor
 
-{{ range $path := .Paths }}
-{{ if $path.Host }}acl host_acl_{{ $path.Backend.Name }} {{ $path.Host | host_name }}{{ end }}
-{{ if $path.Path }}acl url_acl_{{ $path.Backend.Name }} path_beg {{ $path.Path }}{{ end }}
-use_backend {{ $path.Backend.Name }} {{ if or $path.Host $path.Path }}if {{ end }}{{ if $path.Host }}host_acl_{{ $path.Backend.Name }}{{ end }}{{ if $path.Path }}url_acl_{{ $path.Backend.Name }}{{ end }}
-{{ end }}
-{{ if .DefaultBackend }}default_backend default-backend{{ end }}
+	{{ range $path := .Paths }}
+	{{ if $path.Host }}acl host_acl_{{ $path.Backend.Name }} {{ $path.Host | host_name }}{{ end }}
+	{{ if $path.Path }}acl url_acl_{{ $path.Backend.Name }} path_beg {{ $path.Path }}{{ end }}
+	use_backend {{ $path.Backend.Name }} {{ if or $path.Host $path.Path }}if {{ end }}{{ if $path.Host }}host_acl_{{ $path.Backend.Name }}{{ end }}{{ if $path.Path }}url_acl_{{ $path.Backend.Name }}{{ end }}
+	{{ end }}
+	{{ if .DefaultBackend }}default_backend default-backend{{ end }}
 `))
 
 	_ = template.Must(template.New("http-backend").Funcs(funcMap).Parse(`
 {{ range $path := .Paths }}
 backend {{ $path.Backend.Name }}
-{{ if .Sticky }}cookie SERVERID insert indirect nocache{{ end }}
+	{{ if .Sticky }}cookie SERVERID insert indirect nocache{{ end }}
 
-{{ range $rule := $path.Backend.BackendRules }}
-{{ $rule }}
-{{ end }}
+	{{ range $rule := $path.Backend.BackendRules }}
+	{{ $rule }}
+	{{ end }}
 
-{{ range $rule := $path.Backend.RewriteRules }}
-reqrep {{ $rule }}
-{{ end }}
+	{{ range $rule := $path.Backend.RewriteRules }}
+	reqrep {{ $rule }}
+	{{ end }}
 
-{{ range $index, $rule := $path.Backend.HeaderRules }}
-acl ___header_x_{{ $index }}_exists req.hdr({{ $rule | header_name }}) -m found
-http-request add-header {{ $rule }} unless ___header_x_{{ $index }}_exists
-{{ end }}
+	{{ range $index, $rule := $path.Backend.HeaderRules }}
+	acl ___header_x_{{ $index }}_exists req.hdr({{ $rule | header_name }}) -m found
+	http-request add-header {{ $rule }} unless ___header_x_{{ $index }}_exists
+	{{ end }}
 
-{{ range $e := $path.Backend.Endpoints }}
-{{ if $e.ExternalName }}
-{{ if $e.UseDNSResolver }}
-server {{ $e.Name }} {{ $e.ExternalName }}:{{ $e.Port }} {{ if $e.DNSResolver }} {{ if $e.CheckHealth }} check {{ end }} resolvers {{ $e.DNSResolver }} resolve-prefer ipv4 {{ end }}
-{{ else if not $path.Backend.BackendRules }}
-http-request redirect location {{ if .UseSSL }}https://{{ else }}http://{{ end }}{{$e.ExternalName}}:{{ $e.Port }} code 301
-{{ end }}
-{{ else }}
-server {{ $e.Name }} {{ $e.IP }}:{{ $e.Port }} {{ if $e.Weight }}weight {{ $e.Weight }} {{ end }} {{ if .Sticky }}cookie {{ $e.Name }} {{ end }}
-{{ end }}
-{{ end }}
+	{{ range $e := $path.Backend.Endpoints }}
+	{{ if $e.ExternalName }}
+	{{ if $e.UseDNSResolver }}
+	server {{ $e.Name }} {{ $e.ExternalName }}:{{ $e.Port }} {{ if $e.DNSResolver }} {{ if $e.CheckHealth }} check {{ end }} resolvers {{ $e.DNSResolver }} resolve-prefer ipv4 {{ end }}
+	{{ else if not $path.Backend.BackendRules }}
+	http-request redirect location {{ if .UseSSL }}https://{{ else }}http://{{ end }}{{$e.ExternalName}}:{{ $e.Port }} code 301
+	{{ end }}
+	{{ else }}
+	server {{ $e.Name }} {{ $e.IP }}:{{ $e.Port }} {{ if $e.Weight }}weight {{ $e.Weight }} {{ end }} {{ if .Sticky }}cookie {{ $e.Name }} {{ end }}
+	{{ end }}
+	{{ end }}
 {{ end }}
 `))
 
 	_ = template.Must(template.New("tcp-frontend").Funcs(funcMap).Parse(`
 frontend {{ .FrontendName }}
-bind *:{{ .Port }} {{ if .AcceptProxy }}accept-proxy{{ end }} {{ if .SecretName }}ssl no-sslv3 no-tlsv10 no-tls-tickets crt /etc/ssl/private/haproxy/{{ .SecretName }}.pem{{ end }} {{if .ALPNOptions }}{{.ALPNOptions}}{{ end }}
-mode tcp
-default_backend {{ .Backend.Name }}
+	bind *:{{ .Port }} {{ if .AcceptProxy }}accept-proxy{{ end }} {{ if .SecretName }}ssl no-sslv3 no-tlsv10 no-tls-tickets crt /etc/ssl/private/haproxy/{{ .SecretName }}.pem{{ end }} {{if .ALPNOptions }}{{.ALPNOptions}}{{ end }}
+	mode tcp
+	default_backend {{ .Backend.Name }}
 `))
 
 	_ = template.Must(template.New("tcp-backend").Funcs(funcMap).Parse(`
 backend {{ .Backend.Name }}
-mode tcp
+	mode tcp
 
-{{ range $rule := .Backend.BackendRules }}
-{{ $rule }}
-{{ end }}
+	{{ range $rule := .Backend.BackendRules }}
+	{{ $rule }}
+	{{ end }}
 
-{{ if .Sticky }}
-stick-table type ip size 100k expire 30m
-stick on src
-{{ end }}
+	{{ if .Sticky }}
+	stick-table type ip size 100k expire 30m
+	stick on src
+	{{ end }}
 
-{{ range $e := .Backend.Endpoints }}
-{{ if $e.ExternalName }}
-server {{ $e.Name }} {{ $e.ExternalName }}:{{ $e.Port }} {{ if $e.DNSResolver }} {{ if $e.CheckHealth }}check{{ end }} resolvers {{ $e.DNSResolver }} resolve-prefer ipv4{{ end }}
-{{ else }}
-server {{ $e.Name }} {{ $e.IP }}:{{ $e.Port }} {{ if $e.Weight }}weight {{ $e.Weight }}{{ end }}
-{{ end }}
-{{ end }}
+	{{ range $e := .Backend.Endpoints }}
+	{{ if $e.ExternalName }}
+	server {{ $e.Name }} {{ $e.ExternalName }}:{{ $e.Port }} {{ if $e.DNSResolver }} {{ if $e.CheckHealth }}check{{ end }} resolvers {{ $e.DNSResolver }} resolve-prefer ipv4{{ end }}
+	{{ else }}
+	server {{ $e.Name }} {{ $e.IP }}:{{ $e.Port }} {{ if $e.Weight }}weight {{ $e.Weight }}{{ end }}
+	{{ end }}
+	{{ end }}
 `))
 )
