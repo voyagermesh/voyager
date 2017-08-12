50a17fedcacdb6d4169c236012753c8e5faad9eb
tamal <tamal@appscode.com>
2017-08-11 21:42:12 -0700

    added-all

---
+++ pkg/ingress/hostport_test.go
@@ -0,0 +1,201 @@
+package ingress
+
+import (
+	"fmt"
+	"testing"
+
+	api "github.com/appscode/voyager/api"
+	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/fake"
+	apiv1 "k8s.io/client-go/pkg/api/v1"
+	extensions "k8s.io/client-go/pkg/apis/extensions/v1beta1"
+)
+
+func TestHostPortController_IsExistsIsExists(t *testing.T) {
+	testCases := map[*hostPortController]bool{
+		{
+			controller: &controller{
+				Ingress: &api.Ingress{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "foo",
+						Namespace: "bar",
+						Annotations: map[string]string{
+							api.LBType: api.LBTypeHostPort,
+						},
+					},
+				},
+				KubeClient: fake.NewSimpleClientset(
+					&extensions.DaemonSet{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      api.VoyagerPrefix + "foo",
+							Namespace: "bar",
+						},
+					},
+
+					&apiv1.Service{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      api.VoyagerPrefix + "foo",
+							Namespace: "bar",
+						},
+					},
+
+					&apiv1.ConfigMap{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      api.VoyagerPrefix + "foo",
+							Namespace: "bar",
+						},
+					},
+				),
+			},
+		}: true,
+
+		{
+			controller: &controller{
+				Ingress: &api.Ingress{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "foo",
+						Namespace: "bar",
+						Annotations: map[string]string{
+							api.LBType: api.LBTypeHostPort,
+						},
+					},
+				},
+				KubeClient: fake.NewSimpleClientset(
+					&extensions.DaemonSet{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      api.VoyagerPrefix + "fake-foo",
+							Namespace: "bar",
+						},
+					},
+
+					&apiv1.Service{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      api.VoyagerPrefix + "foo",
+							Namespace: "bar",
+						},
+					},
+
+					&apiv1.ConfigMap{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      api.VoyagerPrefix + "foo",
+							Namespace: "bar",
+						},
+					},
+				),
+			},
+		}: false,
+
+		{
+			controller: &controller{
+				Ingress: &api.Ingress{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "foo",
+						Namespace: "bar",
+						Annotations: map[string]string{
+							api.LBType: api.LBTypeHostPort,
+						},
+					},
+				},
+				KubeClient: fake.NewSimpleClientset(
+					&extensions.DaemonSet{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      api.VoyagerPrefix + "foo",
+							Namespace: "bar",
+						},
+					},
+
+					&apiv1.Service{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      api.VoyagerPrefix + "fake-foo",
+							Namespace: "bar",
+						},
+					},
+
+					&apiv1.ConfigMap{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      api.VoyagerPrefix + "foo",
+							Namespace: "bar",
+						},
+					},
+				),
+			},
+		}: false,
+		{
+			controller: &controller{
+				Ingress: &api.Ingress{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "foo",
+						Namespace: "bar",
+						Annotations: map[string]string{
+							api.LBType: api.LBTypeHostPort,
+						},
+					},
+				},
+				KubeClient: fake.NewSimpleClientset(
+					&extensions.DaemonSet{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      api.VoyagerPrefix + "foo",
+							Namespace: "bar",
+						},
+					},
+
+					&apiv1.Service{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      api.VoyagerPrefix + "foo",
+							Namespace: "bar",
+						},
+					},
+
+					&apiv1.ConfigMap{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      api.VoyagerPrefix + "fake-foo",
+							Namespace: "bar",
+						},
+					},
+				),
+			},
+		}: false,
+
+		{
+			controller: &controller{
+				Ingress: &api.Ingress{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "foo",
+						Namespace: "bar",
+						Annotations: map[string]string{
+							api.LBType: api.LBTypeHostPort,
+						},
+					},
+				},
+				KubeClient: fake.NewSimpleClientset(
+					&extensions.DaemonSet{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      api.VoyagerPrefix + "foo",
+							Namespace: "bar",
+						},
+					},
+
+					&apiv1.Service{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      api.VoyagerPrefix + "foo",
+							Namespace: "bar",
+						},
+					},
+
+					&apiv1.ConfigMap{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      api.VoyagerPrefix + "foo",
+							Namespace: "bar",
+						},
+					},
+				),
+			},
+		}: true,
+	}
+
+	for k, v := range testCases {
+		fmt.Println(k.Ingress.Name, k.Ingress.Annotations)
+		assert.Equal(t, v, k.IsExists())
+	}
+}
---
+++ pkg/ingress/loadbalancer_tet.go
@@ -0,0 +1,94 @@
+package ingress
+
+import (
+	"fmt"
+	"testing"
+
+	api "github.com/appscode/voyager/api"
+	"github.com/stretchr/testify/assert"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes/fake"
+	apiv1 "k8s.io/client-go/pkg/api/v1"
+	extensions "k8s.io/client-go/pkg/apis/extensions/v1beta1"
+)
+
+func TestLoadBalancerController_IsExistsIsExists(t *testing.T) {
+	testCases := map[*loadBalancerController]bool{
+		{
+			controller: &controller{
+				Ingress: &api.Ingress{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "foo",
+						Namespace: "bar",
+						Annotations: map[string]string{
+							api.LBType: api.LBTypeLoadBalancer,
+						},
+					},
+				},
+				KubeClient: fake.NewSimpleClientset(
+					&extensions.Deployment{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      api.VoyagerPrefix + "foo",
+							Namespace: "bar",
+						},
+					},
+
+					&apiv1.Service{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      api.VoyagerPrefix + "foo",
+							Namespace: "bar",
+						},
+					},
+
+					&apiv1.ConfigMap{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      api.VoyagerPrefix + "foo",
+							Namespace: "bar",
+						},
+					},
+				),
+			},
+		}: true,
+
+		{
+			controller: &controller{
+				Ingress: &api.Ingress{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "foo",
+						Namespace: "bar",
+						Annotations: map[string]string{
+							api.LBType: api.LBTypeLoadBalancer,
+						},
+					},
+				},
+				KubeClient: fake.NewSimpleClientset(
+					&extensions.Deployment{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      api.VoyagerPrefix + "fakefoo",
+							Namespace: "bar",
+						},
+					},
+
+					&apiv1.Service{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      api.VoyagerPrefix + "foo",
+							Namespace: "bar",
+						},
+					},
+
+					&apiv1.ConfigMap{
+						ObjectMeta: metav1.ObjectMeta{
+							Name:      api.VoyagerPrefix + "foo",
+							Namespace: "bar",
+						},
+					},
+				),
+			},
+		}: false,
+	}
+
+	for k, v := range testCases {
+		fmt.Println(k.Ingress.Name, k.Ingress.Annotations)
+		assert.Equal(t, v, k.IsExists())
+	}
+}
