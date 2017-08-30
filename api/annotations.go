ba1a67f7a88c7223fa41673d4d0b2ff41b643e9f
Tamal Saha <tamal@appscode.com>
2017-08-30 15:37:09 -0700

    Update annotations.go

--- api/annotations.go
+++ api/annotations.go
@@ -107,7 +107,7 @@ const (
 	// https://cbonte.github.io/haproxy-dconv/1.7/configuration.html#4.2-option%20abortonclose
 	// from the list from here
 	// expects a json encoded map
-	// ie: "ingress.appscode.com/default-option": {"http-keep-alive": "true", "dontlognull": "true", "clitcpka": "false"}
+	// ie: "ingress.appscode.com/default-option": '{"http-keep-alive": "true", "dontlognull": "true", "clitcpka": "false"}'
 	// This will be appended in the defaults section of HAProxy as
 	//
 	//   option http-keep-alive
