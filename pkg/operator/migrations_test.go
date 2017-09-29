9beac57d3b4f5e5f700bd532a268c72af7eb2b71
sadlil <sadlil.rhythm@gmail.com>
2017-09-29 12:26:06 +0600

    Add tests for certificate migrations

---
+++ pkg/operator/migrations_test.go
@@ -0,0 +1,48 @@
+package operator
+
+import (
+	"log"
+	"testing"
+
+	api "github.com/appscode/voyager/apis/voyager/v1beta1"
+	"github.com/stretchr/testify/assert"
+	"k8s.io/client-go/kubernetes/fake"
+	apiv1 "k8s.io/client-go/pkg/api/v1"
+)
+
+func TestCertificateMigrations(t *testing.T) {
+	op := &Operator{
+		KubeClient: fake.NewSimpleClientset(),
+	}
+
+	for i, cert := range testInput {
+		log.Println(i, cert.Spec.Domains)
+		ok, err := op.MigrateCertificate(&cert)
+		if assert.True(t, ok) && assert.Nil(t, err) {
+			assert.Equal(t, cert, output[i])
+		}
+	}
+}
+
+var testInput []api.Certificate = []api.Certificate{
+	{
+		Spec: api.CertificateSpec{
+			Provider:                     "http",
+			HTTPProviderIngressReference: apiv1.ObjectReference{Name: "test", Namespace: "default", APIVersion: "voyager.appscode.com/v1beta1"},
+			Domains: []string{"http.appscode.com"},
+		},
+	},
+}
+
+var output []api.Certificate = []api.Certificate{
+	{
+		Spec: api.CertificateSpec{
+			ChallengeProvider: api.ChallengeProvider{
+				HTTP: &api.HTTPChallengeProvider{Ingress: apiv1.ObjectReference{Name: "test", Namespace: "default", APIVersion: "voyager.appscode.com/v1beta1"}},
+			},
+			Domains:            []string{"http.appscode.com"},
+			ACMEUserSecretName: "acme-",
+			Storage:            api.CertificateStorage{Secret: &api.SecretStore{}},
+		},
+	},
+}
