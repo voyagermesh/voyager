ea36395d1d1c2fba5f4cc93eca9f4eb1c98bfebc
tamal <tamal@appscode.com>
2017-10-02 03:49:30 -0700

    Generate ugorji stuff

--- hack/codegen.sh
+++ hack/codegen.sh
@@ -9,6 +9,17 @@ DOCKER_REPO_ROOT="/go/src/$PACKAGE_NAME"
 
 pushd $REPO_ROOT
 
+## Generate ugorji stuff
+docker run --rm -ti -u $(id -u):$(id -g) \
+    -v "$REPO_ROOT":"$DOCKER_REPO_ROOT" \
+    -w "$DOCKER_REPO_ROOT/apis/voyager/v1beta1" \
+    appscode/gengo:canary codecgen -o certificate.generated.go certificate.go
+
+docker run --rm -ti -u $(id -u):$(id -g) \
+    -v "$REPO_ROOT":"$DOCKER_REPO_ROOT" \
+    -w "$DOCKER_REPO_ROOT/apis/voyager/v1beta1" \
+    appscode/gengo:canary codecgen -o ingress.generated.go ingress.go
+
 # Generate defaults
 docker run --rm -ti -u $(id -u):$(id -g) \
     -v "$REPO_ROOT":"$DOCKER_REPO_ROOT" \
