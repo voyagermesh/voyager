308a5cb0062216b00c98a256e9d054cf7d4f42d2
tamal <tamal@appscode.com>
2017-10-06 06:43:41 -0700

    Prepare docs for 4.0.0-rc.6

--- pkg/ingress/create.go
+++ pkg/ingress/create.go
@@ -13,7 +13,7 @@ import (
 )
 
 const (
-	ExporterSidecarTag       = "appscode/voyager:4.0.0-rc.5"
+	ExporterSidecarTag       = "appscode/voyager:4.0.0-rc.6"
 	TLSCertificateVolumeName = "voyager-certdir"
 	ErrorFilesVolumeName     = "voyager-errorfiles"
 	ErrorFilesLocation       = "/srv/voyager/errorfiles"
