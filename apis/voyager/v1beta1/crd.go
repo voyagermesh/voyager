4c9b91dd746a2962bcd18d08b35647bc3de0c978
Tamal Saha <tamal@appscode.com>
2017-10-11 03:06:39 -0700

    Move CRD definition to api folder. (#613)

---
+++ apis/voyager/v1beta1/crd.go
@@ -0,0 +1,47 @@
+package v1beta1
+
+import (
+	"github.com/appscode/voyager/apis/voyager"
+	apiextensions "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func (r Ingress) CustomResourceDefinition() *apiextensions.CustomResourceDefinition {
+	return &apiextensions.CustomResourceDefinition{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   ResourceTypeIngress + "." + SchemeGroupVersion.Group,
+			Labels: map[string]string{"app": "voyager"},
+		},
+		Spec: apiextensions.CustomResourceDefinitionSpec{
+			Group:   voyager.GroupName,
+			Version: SchemeGroupVersion.Version,
+			Scope:   apiextensions.NamespaceScoped,
+			Names: apiextensions.CustomResourceDefinitionNames{
+				Singular:   ResourceNameIngress,
+				Plural:     ResourceTypeIngress,
+				Kind:       ResourceKindIngress,
+				ShortNames: []string{"ing"},
+			},
+		},
+	}
+}
+
+func (c Certificate) CustomResourceDefinition() *apiextensions.CustomResourceDefinition {
+	return &apiextensions.CustomResourceDefinition{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   ResourceTypeCertificate + "." + SchemeGroupVersion.Group,
+			Labels: map[string]string{"app": "voyager"},
+		},
+		Spec: apiextensions.CustomResourceDefinitionSpec{
+			Group:   voyager.GroupName,
+			Version: SchemeGroupVersion.Version,
+			Scope:   apiextensions.NamespaceScoped,
+			Names: apiextensions.CustomResourceDefinitionNames{
+				Singular:   ResourceNameCertificate,
+				Plural:     ResourceTypeCertificate,
+				Kind:       ResourceKindCertificate,
+				ShortNames: []string{"cert"},
+			},
+		},
+	}
+}
