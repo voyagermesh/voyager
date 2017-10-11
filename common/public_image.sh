3b48dbcfa7b6118fbf0a38ec4da9e284ac656137
tamal <tamal@appscode.com>
2017-10-11 01:21:19 -0700

    Squashed 'hack/libbuild/' changes from f1e269a..4c4d915
    4c4d915 Use Docker hub for public images
    
    git-subtree-dir: hack/libbuild
    git-subtree-split: 4c4d9152cafb3cb197ce43cd26af1f403be4b2ff

--- common/public_image.sh
+++ common/public_image.sh
@@ -5,11 +5,11 @@ source $(dirname "${BASH_SOURCE}")/lib.sh
 
 # override this one if you need to change push & pull
 docker_push() {
-	attic_up
+	hub_canary
 }
 
 docker_pull() {
-	attic_pull
+	hub_pull
 }
 
 
