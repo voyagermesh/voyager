d6d5b44d78712c53588b0726a83040dfc98604b8
sadlil <sadlil.rhythm@gmail.com>
2017-10-12 13:57:59 +0600

    Added More tests

--- test/e2e/ingress_tlsauth.go
+++ test/e2e/ingress_tlsauth.go
@@ -3,7 +3,6 @@ package e2e
 import (
 	"crypto/tls"
 	"crypto/x509"
-	"fmt"
 	"io/ioutil"
 	"net"
 	"net/http"
@@ -139,7 +138,7 @@ var _ = Describe("IngressWithTLSAuth", func() {
 			}
 		})
 
-		FIt("Should response HTTP", func() {
+		It("Should response HTTP", func() {
 			By("Getting HTTP endpoints")
 			eps, err := f.Ingress.GetHTTPEndpoints(ing)
 			Expect(err).NotTo(HaveOccurred())
@@ -167,7 +166,6 @@ var _ = Describe("IngressWithTLSAuth", func() {
 		Outer:
 			for _, ep := range eps {
 				vep := strings.TrimLeft(ep[:strings.LastIndex(ep, ":")], "http://")
-				fmt.Println(vep)
 				for _, ip := range ips {
 					if vep == ip {
 						resolved = true
@@ -188,7 +186,6 @@ var _ = Describe("IngressWithTLSAuth", func() {
 			// Wrong Cert
 			tr := getTransportForCert(f.CertManager.CACert(), tlsSecret.Data[apiv1.TLSCertKey], tlsSecret.Data[apiv1.TLSPrivateKeyKey])
 			err = f.Ingress.DoTestRedirectWithTransport(framework.NoRetry, "http.appscode.test", tr, ing, []string{"https://http.appscode.test"}, "GET", "/testpath/hello", func(r *testserverclient.Response) bool {
-				fmt.Println(*r)
 				return Expect(r.Status).Should(Equal(302)) &&
 					Expect(r.ResponseHeader).Should(HaveKey("Location")) &&
 					Expect(r.ResponseHeader.Get("Location")).Should(Equal("https://http.appscode.test/testpath/ok"))
@@ -198,7 +195,234 @@ var _ = Describe("IngressWithTLSAuth", func() {
 			// TLS Auth
 			tr = getTransportForCert(f.CertManager.CACert(), ccrt, ckey)
 			err = f.Ingress.DoHTTPsWithTransport(framework.MaxRetry, "http.appscode.test", tr, ing, []string{"https://http.appscode.test"}, "GET", "/testpath/hello", func(r *testserverclient.Response) bool {
-				fmt.Println(*r)
+				return Expect(r.Status).Should(Equal(http.StatusOK)) &&
+					Expect(r.Method).Should(Equal("GET")) &&
+					Expect(r.Path).Should(Equal("/testpath/hello"))
+			})
+			Expect(err).NotTo(HaveOccurred())
+		})
+	})
+
+	Describe("Create With Header", func() {
+		BeforeEach(func() {
+			if f.Config.CloudProviderName == "minikube" {
+				ing.Annotations[api.LBType] = api.LBTypeHostPort
+			}
+			ing.Spec = api.IngressSpec{
+				FrontendRules: []api.FrontendRule{
+					{
+						Port: intstr.FromInt(443),
+						Auth: &api.AuthOption{
+							TLS: &api.TLSAuth{
+								SecretName:   caSecret.Name,
+								VerifyClient: api.TLSAuthVerifyRequired,
+								ErrorPage:    "https://http.appscode.test/testpath/ok",
+								Headers: map[string]string{
+									"X-SSL":               "%[ssl_fc]",
+									"X-SSL-Client-Verify": "%[ssl_c_verify]",
+									"X-SSL-Client-CN":     "%{+Q}[ssl_c_s_dn(cn)]",
+								},
+							},
+						},
+					},
+				},
+				TLS: []api.IngressTLS{
+					{
+						Ref: &api.LocalTypedReference{
+							Kind: "Secret",
+							Name: tlsSecret.Name,
+						},
+						Hosts: []string{"http.appscode.test"},
+					},
+				},
+				Rules: []api.IngressRule{
+					{
+						Host: "http.appscode.test",
+						IngressRuleValue: api.IngressRuleValue{
+							HTTP: &api.HTTPIngressRuleValue{
+								Paths: []api.HTTPIngressPath{
+									{
+										Path: "/testpath",
+										Backend: api.HTTPIngressBackend{
+											IngressBackend: api.IngressBackend{
+												ServiceName: f.Ingress.TestServerName(),
+												ServicePort: intstr.FromInt(80),
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			}
+		})
+
+		It("Should response HTTP", func() {
+			By("Getting HTTP endpoints")
+			eps, err := f.Ingress.GetHTTPEndpoints(ing)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(len(eps)).Should(BeNumerically(">=", 1))
+
+			ccrt, ckey, err := f.CertManager.NewClientCertPair()
+			Expect(err).NotTo(HaveOccurred())
+
+			if len(f.Config.DumpLocation) > 0 {
+				ioutil.WriteFile(f.Config.DumpLocation+"/ca.crt", f.CertManager.CACert(), os.ModePerm)
+				ioutil.WriteFile(f.Config.DumpLocation+"/client.crt", ccrt, os.ModePerm)
+				ioutil.WriteFile(f.Config.DumpLocation+"/client.key", ckey, os.ModePerm)
+			}
+
+			resolved := false
+			log.Warningln("Domain 'http.appscode.test' must resolve to ips in", eps)
+
+			// Checking for the domain is pointing to the ips found in the endpoints
+			// The IPs and domain must be in /etc/hosts file
+			ips, err := net.LookupHost("http.appscode.test")
+			if err != nil || len(ips) == 0 {
+				Skip("Domain 'http.appscode.test' do not have endpoints")
+			}
+
+		Outer:
+			for _, ep := range eps {
+				vep := strings.TrimLeft(ep[:strings.LastIndex(ep, ":")], "http://")
+				for _, ip := range ips {
+					if vep == ip {
+						resolved = true
+						break Outer
+					}
+				}
+			}
+
+			if !resolved {
+				Skip("Domain 'http.appscode.test' did not point to endpoints")
+			}
+
+			err = f.Ingress.DoHTTPsStatus(framework.NoRetry, "http.appscode.test", ing, eps, "GET", "/testpath/hello", func(r *testserverclient.Response) bool {
+				return true
+			})
+			Expect(err).To(HaveOccurred())
+
+			// TLS Auth
+			tr := getTransportForCert(f.CertManager.CACert(), ccrt, ckey)
+			err = f.Ingress.DoHTTPsWithTransport(framework.MaxRetry, "http.appscode.test", tr, ing, []string{"https://http.appscode.test"}, "GET", "/testpath/hello", func(r *testserverclient.Response) bool {
+				return Expect(r.Status).Should(Equal(http.StatusOK)) &&
+					Expect(r.Method).Should(Equal("GET")) &&
+					Expect(r.Path).Should(Equal("/testpath/hello")) &&
+					Expect(r.RequestHeaders.Get("X-SSL-Client-CN")).Should(Equal("e2e-test")) &&
+					Expect(r.RequestHeaders.Get("X-SSL-Client-Verify")).Should(Equal("0"))
+			})
+			Expect(err).NotTo(HaveOccurred())
+		})
+	})
+
+	Describe("Create Optional Auth", func() {
+		BeforeEach(func() {
+			if f.Config.CloudProviderName == "minikube" {
+				ing.Annotations[api.LBType] = api.LBTypeHostPort
+			}
+			ing.Spec = api.IngressSpec{
+				FrontendRules: []api.FrontendRule{
+					{
+						Port: intstr.FromInt(443),
+						Auth: &api.AuthOption{
+							TLS: &api.TLSAuth{
+								SecretName:   caSecret.Name,
+								VerifyClient: api.TLSAuthVerifyOptional,
+							},
+						},
+					},
+				},
+				TLS: []api.IngressTLS{
+					{
+						Ref: &api.LocalTypedReference{
+							Kind: "Secret",
+							Name: tlsSecret.Name,
+						},
+						Hosts: []string{"http.appscode.test"},
+					},
+				},
+				Rules: []api.IngressRule{
+					{
+						Host: "http.appscode.test",
+						IngressRuleValue: api.IngressRuleValue{
+							HTTP: &api.HTTPIngressRuleValue{
+								Paths: []api.HTTPIngressPath{
+									{
+										Path: "/testpath",
+										Backend: api.HTTPIngressBackend{
+											IngressBackend: api.IngressBackend{
+												ServiceName: f.Ingress.TestServerName(),
+												ServicePort: intstr.FromInt(80),
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			}
+		})
+
+		It("Should response HTTP", func() {
+			By("Getting HTTP endpoints")
+			eps, err := f.Ingress.GetHTTPEndpoints(ing)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(len(eps)).Should(BeNumerically(">=", 1))
+
+			ccrt, ckey, err := f.CertManager.NewClientCertPair()
+			Expect(err).NotTo(HaveOccurred())
+
+			if len(f.Config.DumpLocation) > 0 {
+				ioutil.WriteFile(f.Config.DumpLocation+"/ca.crt", f.CertManager.CACert(), os.ModePerm)
+				ioutil.WriteFile(f.Config.DumpLocation+"/client.crt", ccrt, os.ModePerm)
+				ioutil.WriteFile(f.Config.DumpLocation+"/client.key", ckey, os.ModePerm)
+			}
+
+			resolved := false
+			log.Warningln("Domain 'http.appscode.test' must resolve to ips in", eps)
+
+			// Checking for the domain is pointing to the ips found in the endpoints
+			// The IPs and domain must be in /etc/hosts file
+			ips, err := net.LookupHost("http.appscode.test")
+			if err != nil || len(ips) == 0 {
+				Skip("Domain 'http.appscode.test' do not have endpoints")
+			}
+
+		Outer:
+			for _, ep := range eps {
+				vep := strings.TrimLeft(ep[:strings.LastIndex(ep, ":")], "http://")
+				for _, ip := range ips {
+					if vep == ip {
+						resolved = true
+						break Outer
+					}
+				}
+			}
+
+			if !resolved {
+				Skip("Domain 'http.appscode.test' did not point to endpoints")
+			}
+
+			// No Cert but Cert is optional allow
+			err = f.Ingress.DoHTTPs(framework.NoRetry, "http.appscode.test", "", ing, eps, "GET", "/testpath/hello", func(r *testserverclient.Response) bool {
+				return Expect(r.Status).Should(Equal(http.StatusOK)) &&
+					Expect(r.Method).Should(Equal("GET")) &&
+					Expect(r.Path).Should(Equal("/testpath/hello"))
+			})
+			Expect(err).NotTo(HaveOccurred())
+
+			// Wrong Cert Reject
+			tr := getTransportForCert(f.CertManager.CACert(), tlsSecret.Data[apiv1.TLSCertKey], tlsSecret.Data[apiv1.TLSPrivateKeyKey])
+			err = f.Ingress.DoTestRedirectWithTransport(framework.NoRetry, "http.appscode.test", tr, ing, []string{"https://http.appscode.test"}, "GET", "/testpath/hello", func(r *testserverclient.Response) bool {
+				return false
+			})
+			Expect(err).To(HaveOccurred())
+
+			// TLS Auth
+			tr = getTransportForCert(f.CertManager.CACert(), ccrt, ckey)
+			err = f.Ingress.DoHTTPsWithTransport(framework.MaxRetry, "http.appscode.test", tr, ing, []string{"https://http.appscode.test"}, "GET", "/testpath/hello", func(r *testserverclient.Response) bool {
 				return Expect(r.Status).Should(Equal(http.StatusOK)) &&
 					Expect(r.Method).Should(Equal("GET")) &&
 					Expect(r.Path).Should(Equal("/testpath/hello"))
