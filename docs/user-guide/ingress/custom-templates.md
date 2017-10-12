7c9165602d5d600d707767cdd69204615fbcd872
tamal <tamal@appscode.com>
2017-10-12 13:24:16 -0700

    Prepare docs for 4.0.0-rc.11

--- docs/user-guide/ingress/custom-templates.md
+++ docs/user-guide/ingress/custom-templates.md
@@ -61,7 +61,7 @@ spec:
         - --cloud-config=
         - --ingress-class=
         - --custom-templates=/srv/voyager/custom/*.cfg
-        image: appscode/voyager:4.0.0-rc.10
+        image: appscode/voyager:4.0.0-rc.11
         ports:
         - containerPort: 56790
           name: http
--- pkg/ingress/create.go
+++ pkg/ingress/create.go
@@ -13,7 +13,7 @@ import (
 )
 
 const (
-	ExporterSidecarTag       = "appscode/voyager:4.0.0-rc.10"
+	ExporterSidecarTag       = "appscode/voyager:4.0.0-rc.11"
 	TLSCertificateVolumeName = "voyager-certdir"
 	ErrorFilesVolumeName     = "voyager-errorfiles"
 	ErrorFilesLocation       = "/srv/voyager/errorfiles"
