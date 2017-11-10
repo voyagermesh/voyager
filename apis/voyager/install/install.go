86ed386d5149fbb3dc8927be0335bbc411f0e3b8
tamal <tamal@appscode.com>
2017-11-10 01:49:56 -0800

    Change pkg alias

--- apis/voyager/install/install.go
+++ apis/voyager/install/install.go
@@ -1,8 +1,8 @@
 package install
 
 import (
-	sapi "github.com/appscode/voyager/apis/voyager"
-	"github.com/appscode/voyager/apis/voyager/v1beta1"
+	"github.com/appscode/voyager/apis/voyager"
+	api "github.com/appscode/voyager/apis/voyager/v1beta1"
 	"k8s.io/apimachinery/pkg/apimachinery/announced"
 	"k8s.io/apimachinery/pkg/apimachinery/registered"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -13,13 +13,13 @@ import (
 func Install(groupFactoryRegistry announced.APIGroupFactoryRegistry, registry *registered.APIRegistrationManager, scheme *runtime.Scheme) {
 	if err := announced.NewGroupMetaFactory(
 		&announced.GroupMetaFactoryArgs{
-			GroupName:                  sapi.GroupName,
-			VersionPreferenceOrder:     []string{v1beta1.SchemeGroupVersion.Version},
+			GroupName:                  voyager.GroupName,
+			VersionPreferenceOrder:     []string{api.SchemeGroupVersion.Version},
 			RootScopedKinds:            sets.NewString("CustomResourceDefinition"),
-			AddInternalObjectsToScheme: sapi.AddToScheme,
+			AddInternalObjectsToScheme: voyager.AddToScheme,
 		},
 		announced.VersionToSchemeFunc{
-			v1beta1.SchemeGroupVersion.Version: v1beta1.AddToScheme,
+			api.SchemeGroupVersion.Version: api.AddToScheme,
 		},
 	).Announce(groupFactoryRegistry).RegisterAndEnable(registry, scheme); err != nil {
 		panic(err)
