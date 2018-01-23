3ceb484dd4018369a8fbd9e02186564504d28b63
Jefferson Girão <jefferson@girao.net>
2018-01-23 13:57:16 +0100

    Removing host_name test case since the filter does not exist

--- pkg/haproxy/template_test.go
+++ pkg/haproxy/template_test.go
@@ -28,23 +28,6 @@ hello
 	assert.Equal(t, exp, buf.String())
 }
 
-func TestHostNameFilter(t *testing.T) {
-	tpl := template.Must(template.New("").Funcs(funcMap).Parse(`
-{{ .val | host_name }}
-{{ .val2 | host_name }}
-`))
-	var buf bytes.Buffer
-	tpl.Execute(&buf, map[string]string{
-		"val":  "appscode.com",
-		"val2": "*.appscode.com",
-	})
-	exp := `
-hdr(host) -i appscode.com
-hdr_end(host) -i .appscode.com
-`
-	assert.Equal(t, exp, buf.String())
-}
-
 func TestTemplate(t *testing.T) {
 	si := &SharedInfo{
 		DefaultBackend: &Backend{
