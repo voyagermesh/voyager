23b64f9c74ae2d33941f71d42497ae6bc6813b36
tamal <tamal@appscode.com>
2018-01-28 02:53:48 -0800

    Add HAProxy 1.8.3

---
+++ hack/docker/haproxy/1.8.3/setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -eou pipefail
+
+GOPATH=$(go env GOPATH)
+REPO_ROOT=$GOPATH/src/github.com/appscode/voyager
+
+source "$REPO_ROOT/hack/libbuild/common/public_image.sh"
+
+detect_tag $REPO_ROOT/dist/.tag
+
+IMG=haproxy
+TAG=1.8.3-$TAG
+
+build() {
+	pushd $(dirname "${BASH_SOURCE}")
+	cp $REPO_ROOT/dist/voyager/voyager-alpine-amd64 voyager
+	chmod +x voyager
+	local cmd="docker build -t appscode/$IMG:$TAG ."
+	echo $cmd; $cmd
+	rm voyager
+	popd
+}
+
+binary_repo $@
