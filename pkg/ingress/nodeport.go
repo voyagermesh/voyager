db698d391366477242035850f684cd307cdf3a64
diptadas <dipta@appscode.com>
2018-02-07 12:43:42 +0600

    Assigned deployment replicas initially

--- pkg/ingress/nodeport.go
+++ pkg/ingress/nodeport.go
@@ -445,9 +445,13 @@ func (c *nodePortController) ensurePods() (*apps.Deployment, kutil.VerbType, err
 
 		obj.Labels = c.Ingress.OffshootLabels()
 		obj.ObjectMeta = c.ensureOwnerReference(obj.ObjectMeta)
-		obj.Spec.Replicas = types.Int32P(c.Ingress.Replicas())
 		obj.Spec.Selector = &metav1.LabelSelector{
 			MatchLabels: c.Ingress.OffshootLabels(),
+		}
+
+		// assign number of replicas for initial creation only
+		if *obj.Spec.Replicas == 0 {
+			obj.Spec.Replicas = types.Int32P(c.Ingress.Replicas())
 		}
 
 		// pod annotations
