6f5f54e1596db5f772a9c8087d73fafaf6ca7540
tahsinrahman <tahsynx@gmail.com>
2018-03-07 17:41:44 +0600

    test

--- hack/concourse/test.sh
+++ hack/concourse/test.sh
@@ -3,14 +3,12 @@
 set -x -e
 
 apk --no-cache add curl bash
-pwd
-ls
-#curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-# chmod +x ./kubectl
-#mv ./kubectl /usr/local/bin/kubectl
-#
-#curl -LO https://cdn.rawgit.com/Mirantis/kubeadm-dind-cluster/master/fixed/dind-cluster-v1.9.sh
-#chmod +x dind-cluster-v1.9.sh
-#./dind-cluster-v1.9.sh up
-#
-#kubectl get nodes
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
+ chmod +x ./kubectl
+mv ./kubectl /usr/local/bin/kubectl
+
+curl -LO https://cdn.rawgit.com/Mirantis/kubeadm-dind-cluster/master/fixed/dind-cluster-v1.9.sh
+chmod +x dind-cluster-v1.9.sh
+./dind-cluster-v1.9.sh up
+
+kubectl get nodes
