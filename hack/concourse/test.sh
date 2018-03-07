54c257e2ace241ddb93edfb45ed592f9cc6e3932
tahsinrahman <tahsynx@gmail.com>
2018-03-07 19:09:48 +0600

    test

--- hack/concourse/test.sh
+++ hack/concourse/test.sh
@@ -2,7 +2,11 @@
 
 set -x -e
 
+source /docker-lib.sh
+start_docker
+
 apk --no-cache add curl bash
+
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
  chmod +x ./kubectl
 mv ./kubectl /usr/local/bin/kubectl
@@ -10,5 +14,6 @@ mv ./kubectl /usr/local/bin/kubectl
 curl -LO https://cdn.rawgit.com/Mirantis/kubeadm-dind-cluster/master/fixed/dind-cluster-v1.9.sh
 chmod +x dind-cluster-v1.9.sh
 ./dind-cluster-v1.9.sh up
+
 
 kubectl get nodes
