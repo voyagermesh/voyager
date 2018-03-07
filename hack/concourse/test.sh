1418fbb3c323888072da6354e0bdfc1af9dfb290
tahsinrahman <tahsynx@gmail.com>
2018-03-07 17:31:01 +0600

    test

--- hack/concourse/test.sh
+++ hack/concourse/test.sh
@@ -2,10 +2,9 @@
 
 set -x -e
 
+apk add curl bash
 pwd
 ls
-#apk update
-#apk add curl bash
 #curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 # chmod +x ./kubectl
 #mv ./kubectl /usr/local/bin/kubectl
