d39666c26e25bc575014af755d23324b0c5a429c
tahsinrahman <tahsynx@gmail.com>
2018-03-09 22:03:44 +0600

    test

--- hack/concourse/test.sh
+++ hack/concourse/test.sh
@@ -2,11 +2,6 @@
 
 set -x -e
 
-apk update
-apk add python py-pip
-pip install git+https://github.com/ellisonbg/antipackage.git#egg=antipackage
-pip install pyyaml
-
 mv kubeconfig/kubeconfig-0.0.1/* /
 ls /
 ./src/github.com/appscode/voyager/hack/make.py test minikube --kubeconfig=/kubeconfig
