a07be4d47b6d56f81f393e1d08404e6d9a327472
tahsinrahman <tahsynx@gmail.com>
2018-03-13 23:14:56 +0600

    added unit tests

--- hack/concourse/unit_test.sh
+++ hack/concourse/unit_test.sh
@@ -3,8 +3,7 @@
 set -x -e
 
 ROOT_DIR=$(pwd)
-mv kubeconfig/kubeconfig-0.0.1/* /
 mkdir -p $GOPATH/src/github.com/appscode
 cp -r voyager $GOPATH/src/github.com/appscode
 cd $GOPATH/src/github.com/appscode/voyager/hack
-./make.py test minikube --kubeconfig=/kubeconfig
+./make.py test unit
