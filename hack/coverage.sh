a3475e50c220c4aa03ea4543e9c236e3a797aa22
tamal <tamal@appscode.com>
2018-03-13 12:46:47 -0700

    Add travis.yaml

---
+++ hack/coverage.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -eou pipefail
+
+GOPATH=$(go env GOPATH)
+REPO_ROOT="$GOPATH/src/github.com/appscode/voyager"
+
+pushd $REPO_ROOT
+
+echo "" > coverage.txt
+
+for d in $(go list ./... | grep -v vendor); do
+    go test -v -race -coverprofile=profile.out -covermode=atomic "$d"
+    if [ -f profile.out ]; then
+        cat profile.out >> coverage.txt
+        rm profile.out
+    fi
+done
+
+popd
