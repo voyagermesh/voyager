bd377c9dd113d20a9e53874e5d652bde9aa52848
tahsinrahman <tahsynx@gmail.com>
2018-03-20 22:54:40 +0600

    concourse configs

--- hack/concourse/e2e_test.sh
+++ hack/concourse/e2e_test.sh
@@ -2,8 +2,24 @@
 
 set -x -e
 
-mv kubeconfig/kubeconfig-0.0.1/* /
 mkdir -p $GOPATH/src/github.com/appscode
 cp -r voyager $GOPATH/src/github.com/appscode
 cd $GOPATH/src/github.com/appscode/voyager/hack
-./make.py test minikube --kubeconfig=/kubeconfig
+
+NAME=voyager$(git rev-parse HEAD) #name of the cluster
+
+cat > cred.json <<EOF
+{
+	"token" : "$TOKEN"
+}
+EOF
+
+pharmer create credential --from-file=cred.json --provider=DigitalOcean cred
+pharmer create cluster $NAME --provider=digitalocean --zone=nyc3 --nodes=2gb=1 --credential-uid=cred --kubernetes-version=v1.9.0
+pharmer apply $NAME
+pharmer use cluster $NAME
+kubectl get nodes
+
+./make.py test minikube
+
+pharmer delete cluster $NAME
