6a7a1652d715a3adb74fa0834c3f53b3e3972cbf
Md. Tahsin Rahman <tahsynx@gmail.com>
2018-04-22 01:32:55 +0600

    concourse configs (#946)

---
+++ hack/concourse/e2e_test_pr.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -x
+
+mkdir -p $GOPATH/src/github.com/appscode
+cp -r pull-request $GOPATH/src/github.com/appscode/voyager
+cd $GOPATH/src/github.com/appscode/voyager/hack
+
+NAME=voyager$(git rev-parse HEAD) #name of the cluster
+
+cat > cred.json <<EOF
+{
+	"token" : "$TOKEN"
+}
+EOF
+
+function cleanup {
+    pharmer get cluster
+    pharmer delete cluster $NAME
+    pharmer get cluster
+    sleep 600
+    pharmer apply $NAME
+    pharmer get cluster
+
+#    for debugging cluster
+#    for f in $(find ~/.pharmer -type f)
+#    do
+#        echo ------$f------; cat $f; echo; echo;
+#    done
+}
+trap cleanup EXIT
+
+pharmer create credential --from-file=cred.json --provider=DigitalOcean cred
+pharmer create cluster $NAME --provider=digitalocean --zone=nyc3 --nodes=2gb=1 --credential-uid=cred --kubernetes-version=v1.9.0
+pharmer apply $NAME
+pharmer use cluster $NAME
+kubectl get nodes
+
+#for debugging cluster
+#for f in $(find ~/.pharmer -type f)
+#do
+#    echo ------$f------; cat $f; echo; echo;
+#done
+
+./make.py test e2e -cloud-provider=baremetal
