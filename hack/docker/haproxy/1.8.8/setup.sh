dbb36270b82eb6171ab12167db9d042fd537e704
diptadas <dipta670@gmail.com>
2018-05-08 12:52:55 +0600

    updated stretch image

--- hack/docker/haproxy/1.8.8/setup.sh
+++ hack/docker/haproxy/1.8.8/setup.sh
@@ -16,9 +16,11 @@ build() {
 	pushd $(dirname "${BASH_SOURCE}")
 	cp $REPO_ROOT/dist/voyager/voyager-linux-amd64 voyager
 	chmod +x voyager
+	# download socklog (`socklog` not available for `stretch`, use `jessie` deb instead)
+	curl -o socklog.deb http://ftp.us.debian.org/debian/pool/main/s/socklog/socklog_2.1.0-8_amd64.deb
 	local cmd="docker build -t appscode/$IMG:$TAG ."
 	echo $cmd; $cmd
-	rm voyager
+	rm voyager socklog.deb
 	popd
 }
 
