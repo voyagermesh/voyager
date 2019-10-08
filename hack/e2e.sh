89e371787bc7380f2d5932c07e27b22c1de54996
Tamal Saha <tamal@appscode.com>
2019-10-08 08:05:42 -0700

    Run e2e tests on Github actions
    Signed-off-by: Tamal Saha <tamal@appscode.com>

---
+++ hack/e2e.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Copyright 2019 AppsCode Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -eou pipefail
+
+export CGO_ENABLED=0
+export GO111MODULE=on
+export GOFLAGS="-mod=vendor"
+
+GINKGO_ARGS=${GINKGO_ARGS:-}
+TEST_ARGS=${TEST_ARGS:-}
+DOCKER_REGISTRY=${DOCKER_REGISTRY:-}
+
+echo "Running e2e tests:"
+cmd="ginkgo -r --v --progress --trace --noisyPendings=false ${GINKGO_ARGS} test -- --docker-registry=${DOCKER_REGISTRY} ${TEST_ARGS}"
+echo $cmd; $cmd
