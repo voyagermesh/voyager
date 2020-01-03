voyagermesh/voyager

commit 22d54f40904e19dfcacca891ecfdffa743eb262f

Fix Linter Issues (#1448)

pkg/ingress/loadbalancer.go
@@ -123,7 +123,10 @@ func (c *loadBalancerController) Reconcile() error {
 	}
 
 	// Ensure service account
-	c.reconcileRBAC()
+	err := c.reconcileRBAC()
+	if err != nil {
+		return err
+	}
 
 	if vt, err := c.ensurePods(); err != nil {
 		c.recorder.Eventf(
@@ -167,7 +170,9 @@ func (c *loadBalancerController) Reconcile() error {
 		)
 	}
 
-	go c.updateStatus()
+	go func() {
+		log.Fatal(c.updateStatus())
+	}()
 
 	if c.Ingress.Stats() {
 		if _, vt, err := c.ensureStatsService(); err != nil { // Error ignored intentionally
@@ -263,7 +268,6 @@ func (c *loadBalancerController) Delete() {
 	if err := c.ensureStatsServiceDeleted(); err != nil {
 		c.logger.Errorln(err)
 	}
-	return
 }
 
 func (c *loadBalancerController) ensureService() (*core.Service, kutil.VerbType, error) {
@@ -348,7 +352,7 @@ func (c *loadBalancerController) ensureService() (*core.Service, kutil.VerbType,
 		// ExternalTrafficPolicy
 		if c.Ingress.KeepSourceIP() {
 			switch c.cfg.CloudProvider {
-			case "gce", "gke", "azure", "acs", "aks", "metallb":
+			case "gce", api.ProviderGKE, "azure", "acs", "aks", "metallb":
 				// https://github.com/appscode/voyager/issues/276
 				// ref: https://kubernetes.io/docs/tasks/services/source-ip/#source-ip-for-services-with-typeloadbalancer
 				obj.Spec.ExternalTrafficPolicy = core.ServiceExternalTrafficPolicyTypeLocal
@@ -360,7 +364,7 @@ func (c *loadBalancerController) ensureService() (*core.Service, kutil.VerbType,
 
 		// LoadBalancerIP
 		switch c.cfg.CloudProvider {
-		case "gce", "gke", "azure", "acs", "aks", "openstack", "metallb", "digitalocean", "linode":
+		case "gce", api.ProviderGKE, "azure", "acs", "aks", "openstack", "metallb", "digitalocean", "linode":
 			if ip := c.Ingress.LoadBalancerIP(); ip != nil {
 				obj.Spec.LoadBalancerIP = ip.String()
 			}

commit c8e072a3e23aa3992f45ddf8b0f3754ebaf33cb4

Exit if UpdateStatus returns error.
Regression was introduced in #1448 . Fixes #1471

Signed-off-by: Tamal Saha <tamal@appscode.com>

pkg/ingress/loadbalancer.go
@@ -40,6 +40,7 @@ import (
 	kext_cs "k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/typed/apiextensions/v1beta1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/client-go/kubernetes"
 	core_listers "k8s.io/client-go/listers/core/v1"
@@ -171,7 +172,7 @@ func (c *loadBalancerController) Reconcile() error {
 	}
 
 	go func() {
-		log.Fatal(c.updateStatus())
+		utilruntime.Must(c.updateStatus())
 	}()
 
 	if c.Ingress.Stats() {