ea232e459f6fdfb8a5aa705c494ba076e77e33d5
Tamal Saha <tamal@appscode.com>
2021-04-23 06:50:16 -0700

    Update license header (#1582)
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- apis/voyager/v1beta1/crd.go
+++ apis/voyager/v1beta1/crd.go
@@ -1,5 +1,5 @@
 /*
-Copyright The Voyager Authors.
+Copyright AppsCode Inc. and Contributors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -17,7 +17,7 @@ limitations under the License.
 package v1beta1
 
 import (
-	"voyagermesh.dev/voyager/api/crds"
+	"voyagermesh.dev/voyager/crds"
 
 	"kmodules.xyz/client-go/apiextensions"
 )
--- hack/docker/haproxy/1.7.2/runit.sh
+++ hack/docker/haproxy/1.7.2/runit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright The Voyager Authors.
+# Copyright AppsCode Inc. and Contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
